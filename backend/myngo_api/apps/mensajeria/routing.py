6524a43df0e1a3a811d88ceff33840dbe52c580e
carmennsss <carmentamdon@gmail.com>
2026-04-26 13:42:02 +0200

    intentando arreglar 2

--- backend/myngo_api/apps/mensajeria/routing.py
+++ backend/myngo_api/apps/mensajeria/routing.py
@@ -2,7 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    # Usamos re_path sin ancla inicial estricta para ser ultra-permisivos con las barras /
     re_path(r'ws/chat/(?P<room_id>\d+)/?', consumers.ChatConsumer.as_asgi()),
     re_path(r'ws/presence/?', consumers.PresenceConsumer.as_asgi()),
     re_path(r'ws/chat-notificaciones/?', consumers.NotificacionesChatConsumer.as_asgi()),
