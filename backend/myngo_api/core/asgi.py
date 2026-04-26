fa07da15c6857b3138141e3f12827e1bdbf83b3c
Ainhoa Gómez Toro <ainhoagomeztoro@gmail.com>
2026-04-26 15:08:12 +0200

    Update asgi.py

--- backend/myngo_api/core/asgi.py
+++ backend/myngo_api/core/asgi.py
@@ -8,6 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(BASE_DIR / 'apps'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
