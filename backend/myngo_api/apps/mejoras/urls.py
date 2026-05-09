d536e3857b10a6ff8545f58c50d15fffb6616e38
Ainhoa Gómez Toro <ainhoagomeztoro@gmail.com>
2026-05-09 19:07:19 +0200

    dcas

--- backend/myngo_api/apps/mejoras/urls.py
+++ backend/myngo_api/apps/mejoras/urls.py
@@ -7,6 +7,7 @@ from .views import (
     CatalogoMejorasGlobales,
     ComprarMejoraView,
     EquipacionMejorasGlobales,
+    EquipacionMejoraComunidad,
     GestionCatalogoComunidad,
     MisMejorasView,
     PeticionMejoraCreate,
