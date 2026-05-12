6b390c05cf6fdd30c809cbbd4b1ca7ac094bbce8
carmennsss <carmentamdon@gmail.com>
2026-05-12 20:46:04 +0200

    Update settings.py

--- backend/myngo_api/core/settings.py
+++ backend/myngo_api/core/settings.py
@@ -82,8 +82,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
     "accept",
