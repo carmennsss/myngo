b45662ae0eccd6f109fa27cc035cd36829530da1
carmennsss <carmentamdon@gmail.com>
2026-05-12 18:42:10 +0200

    workerss

--- frontend/myngo_app/lib/utils/configuracion.dart
+++ frontend/myngo_app/lib/utils/configuracion.dart
@@ -8,7 +8,7 @@ class Configuracion {
 
   /// La dirección base para las conexiones de WebSockets.
   /// Si usas 'runserver' o 'daphne' en el EC2, el puerto suele ser el 8000.
-  static const String wsUrl = 'wss://107.20.99.104/ws'; // Se recomienda WSS para evitar bloqueos
+  static const String wsUrl = 'wss://forget-resulting-slides-momentum.trycloudflare.com/ws'; // Se recomienda WSS para evitar bloqueos
   //static const String wsUrl = 'ws://localhost:8000/ws';
 
 }
